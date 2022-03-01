350a2f583a50f8ba008c575aafdb4d93c0ad0184
Zoli Francia <zoli.francia@gmail.com>
2022-03-01 17:26:42 +0100

    WEEK2

---
+++ WEEK2.cpp
@@ -0,0 +1,80 @@
+//g++ WEEK2.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o week2 `fltk-config --ldflags --use-images`
+#include "Simple_window.h"
+#include "Graph.h"
+#include "std_lib_facilities.h"
+
+int main()
+{
+	try
+	{
+		using namespace Graph_lib;
+		
+		Simple_window win{Point{100,100}, 800, 1000, "My Window"};
+		
+		int sizex=800;
+		int sizey=1000;
+		int gridx=100;
+		int gridy=100;
+		
+		Lines grid;
+		
+		for (int i = gridx; i < sizex; i += gridx)
+		{
+			grid.add(Point{i,0}, Point{i,800});
+		}
+		for (int j = gridy; j < sizey; j += gridy)
+		{
+			grid.add(Point{0,j}, Point{800,j});
+		}
+		
+		
+		Vector_ref<Rectangle> rect;
+		for(int i = 0; i <= 8; i++)
+		{
+			rect.push_back(new Rectangle{Point{i*100, i*100}, 100, 100});
+			rect[i].set_fill_color(Color::red);
+			win.attach(rect[i]);
+		}
+		
+		
+		
+		Image lion1 (Point{600,200}, "sealion.jpeg");
+		Image lion2 (Point{0,200}, "sealion.jpeg");
+		Image lion3 (Point{400,600}, "sealion.jpeg");
+		
+		win.attach(grid);
+		win.attach(lion1);
+		win.attach(lion2);
+		win.attach(lion3);
+		
+		Image finn (Point{100,0}, "finn.jpeg");
+		win.attach(finn);
+		for(int i = 0; i < 12; i++)
+		{
+			win.wait_for_button();
+			if(i%2==0)
+			{
+				finn.move(100,0);
+			}
+			else
+			{
+				finn.move(0,100);
+			}
+			win.attach(finn);
+			
+		}	
+		
+		win.wait_for_button();
+	}
+	catch(exception& e)
+	{
+		cout << "Error\n";
+		return 1;
+	}
+	catch(...)
+	{
+		cout << "Unknown error\n";
+		return 2;
+	}
+}
+
